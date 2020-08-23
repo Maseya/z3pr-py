649b86d7470381a07c71ba6c6653055f900fc9e3
Nelson Garcia <ngarcia@ipac.caltech.edu>
2020-08-22 22:13:52 -0700

    Add JSON to pypi package data

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="maseya-z3pr",
-    version="1.0.0.dev2",
+    version="1.0.0.dev4",
     author="Nelson Garcia",
     author_email="swr.ngarcia@gmail.com",
     description="Randomize palette data for Legend of Zelda: A Link to the Past.",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/maseya/z3pr-py",
     packages=setuptools.find_packages(),
+    package_data={"maseya.z3pr": ["data/*.json"]},
     classifiers=[
         "Development Status :: 1 - Planning",
         "Environment :: Console",
